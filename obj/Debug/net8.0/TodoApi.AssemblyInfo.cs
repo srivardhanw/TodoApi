adcd28a304335ad0080768237224495f0cebacb5
Srivardhan <srivardhan@aaamt.in>
2025-07-15 12:03:18 +0530

    Used MS SQL using Dapper instead of in-memory List

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b58fe099d20628e58733fc37611a0e92cb7215d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3143848c4c9191c01162ff766138b0caf52e627a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
